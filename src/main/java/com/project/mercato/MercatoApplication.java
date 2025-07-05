bf5818252e0044dc5b728bbabb4bb03a6fd6f58b
Gustavo <kkowakskigustavo@gmail.com>
2025-07-05 13:29:15 -0300

    chore: estrutura inicial do projeto gerada com Spring Initializr

---
+++ src/main/java/com/project/mercato/MercatoApplication.java
@@ -0,0 +1,13 @@
+package com.project.mercato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mercato/MercatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mercato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
